c1d0fbe162d8a07026aaa5d052a1337b942264fe
ChicoLindao <chicolima1996@gmail.com>
2024-12-11 19:18:44 -0300

    feat: Section Products

--- backend/api/product/product-by-category.php
+++ backend/api/product/product-by-category.php
@@ -2,12 +2,12 @@
 include_once '../../config.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $category_id = $_GET['category_id'];
+    $category_id = $_GET['id'];
 
-    $sql = "SELECT * FROM products WHERE category_id = ?";
+    $sql = "SELECT * FROM products WHERE id_category = ?";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$category_id]);
-    $products = $stmt->fetchAll();
+    $products = $stmt->fetchAll(PDO::FETCH_OBJ);
 
     echo json_encode($products);
 }
