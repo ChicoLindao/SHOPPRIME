d84a748923f148f1fd26bf71c9d88522f9d08bd8
ChicoLindao <chicolima1996@gmail.com>
2024-12-12 00:52:37 -0300

    Release

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Usuario Admin:
 Email > adm@adm.com
 
 Senha > P@ssword
+
+caminho db > http://localhost/shopprime/backend/SQL/init_db.php
--- backend/SQL/shopprime.sql
+++ backend/SQL/shopprime.sql
@@ -1,54 +1,155 @@
-DROP DATABASE IF EXISTS `shopprime`;
-CREATE SCHEMA IF NOT EXISTS `shopprime`;
-USE `shopprime`;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12/12/2024 às 04:50
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
 
-CREATE TABLE `users` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(255) NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `address` VARCHAR(255),
-    `photo` VARCHAR(255),
-    `role` ENUM("USER", "ADMIN") DEFAULT 'USER',
-    PRIMARY KEY (`id`)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `shopprime`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `categories`
+--
 
 CREATE TABLE `categories` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
-);
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Celular'),
+(2, 'Assinaturas'),
+(3, 'Jogos para Celular'),
+(4, 'Jogos para Computador');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `products`
+--
 
 CREATE TABLE `products` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
-    `price` DECIMAL(10,2) NOT NULL,
-    `img` VARCHAR(255) NOT NULL,
-    `id_category` INT NOT NULL,
-    FOREIGN KEY (`id_category`) REFERENCES `categories`(`id`),
-    PRIMARY KEY (`id`)
-);
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `id_category` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- CREATE TABLE `pedidos` (
---     `id` INT NOT NULL AUTO_INCREMENT,
---     `id_cliente` INT NOT NULL,
---     `total` DECIMAL(10,2),
---     PRIMARY KEY (`id`),
---     FOREIGN KEY (`id_cliente`) REFERENCES `clientes`(`id`)
--- );
+--
+-- Despejando dados para a tabela `products`
+--
 
--- CREATE TABLE `produtos_pedidos` (
---     `id_pedido` INT NOT NULL,
---     `id_produto` INT NOT NULL,
---     PRIMARY KEY (`id_pedido`, `id_produto`),
---     FOREIGN KEY (`id_pedido`) REFERENCES `pedidos`(`id`),
---     FOREIGN KEY (`id_produto`) REFERENCES `produtos`(`id`)
--- );
+INSERT INTO `products` (`id`, `name`, `price`, `img`, `id_category`) VALUES
+(2, 'Lol', 100.00, './backend/storage/products/18c66e2a3dc952b85100b12aa15a208d.jpg', 4),
+(3, 'Valorant', 240.00, './backend/storage/products/c965555ca18143d82fc00f285fb0d62c.jpg', 4),
+(4, 'Wild Rift', 120.00, './backend/storage/products/90d01ca6dfbfccfa561f22f9e640a3b7.jpg', 3),
+(5, 'PUBG Mobile', 50.00, './backend/storage/products/a0bf6ec2252ad159d0b782be39f02369.jpg', 3),
+(6, 'Google Play', 30.00, './backend/storage/products/babd0e4760ff2e355c0a28d3a4e04b10.jpg', 1),
+(7, 'Apple', 50.00, './backend/storage/products/fcd7f2080fd38bf66de1416ba82def39.jpg', 1),
+(8, 'Xbox Live Gold', 200.00, './backend/storage/products/2a0784c42cdef04070109fac6b516a78.jpg', 2),
+(9, 'Netflix', 110.00, './backend/storage/products/576f3cbf04e2d5c60b790f1fbbe5b751.jpg', 2);
 
-INSERT INTO `users` (`email`, `name`, `password`, `role`) VALUES ('adm@adm.com','Administrador', '$2y$10$xTUSv/k4BovdEzJCnIKU3.U4KtfP2N3J3ajJzSRRJaKrpYMHOSfL.', "ADMIN"); -- P@ssword
+-- --------------------------------------------------------
 
-INSERT INTO `categories` (id, name) VALUES
-(1, "Celular"),
-(2, "Assinaturas"),
-(3, "Jogos para Celular"),
-(4, "Jogos para Computador");
+--
+-- Estrutura para tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `role` enum('USER','ADMIN') DEFAULT 'USER'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `name`, `password`, `address`, `photo`, `role`) VALUES
+(1, 'adm@adm.com', 'Administrador', '$2y$10$xTUSv/k4BovdEzJCnIKU3.U4KtfP2N3J3ajJzSRRJaKrpYMHOSfL.', NULL, NULL, 'ADMIN'),
+(2, 'chicolima1996@gmail.com', 'FRANCISCO S LIMA', '$2y$10$CS37NaVb5wuKfqT8GXPUjOMIqaMlXHF/acZAKH0LKtZdEq19BiX2.', NULL, NULL, 'USER');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_category` (`id_category`);
+
+--
+-- Índices de tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
